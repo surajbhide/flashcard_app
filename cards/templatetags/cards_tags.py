9d20b985a5c5c495bea9d8906bd5c0a5b52a2bdf
Suraj Bhide <suraj.bhide+git@gmail.com>
2022-07-28 14:44:58 -0500

    finished project v1

---
+++ cards/templatetags/cards_tags.py
@@ -0,0 +1,17 @@
+from django import template
+
+from cards.models import Card, BOXES
+
+register = template.Library()
+
+@register.inclusion_tag("box_links.html")
+def boxes_as_links():
+    boxes = []
+    for box_num in BOXES:
+        card_count = Card.objects.filter(box=box_num).count()
+        boxes.append({
+            "number" : box_num,
+            "card_count" : card_count,
+        })
+
+    return {"boxes" : boxes}
---
+++ cards/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+#from django.views.generic import TemplateView
+from . import views
+
+urlpatterns = [
+    # path("", TemplateView.as_view(template_name='cards/base.html'), name="home"),
+    path("", views.CardListView.as_view(), name="card-list"),
+    path("new", views.CardCreateView.as_view(), name="card-create"),
+    path("edit/<int:pk>", views.CardUpdateView.as_view(), name="card-update"),
+    path("box/<int:box_num>", views.BoxView.as_view(), name="box"),
+]
